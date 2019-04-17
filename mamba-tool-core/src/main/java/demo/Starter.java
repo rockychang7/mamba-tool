6857c3f4500cd9124ee553acaef2088ca6b5be8f
JoeBig7 <1479762535@qq.com>
2019-04-17 11:27:26 +0800

    first commit

---
+++ mamba-tool-core/src/main/java/demo/Starter.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * @version 1.0
+ * @author: JoeBig7
+ * @date: 2019/4/17 11:19
+ * @description mamba-tool 个人工具类项目启动了。。。
+ */
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("hello world, mamba-tool is starting!!!");
+    }
+
+}
