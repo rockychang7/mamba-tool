b1bde9ef43ae815ff3b0d43a689219415f4657fe
JoeBig7 <1479762535@qq.com>
2021-03-04 18:04:02 +0800

    :refactor: modify code structure

---
+++ mamba-tool-core/src/main/java/com/mamba/core/util/ExcelDataVo.java
@@ -0,0 +1,51 @@
+package com.mamba.core.util;
+
+/**
+ * @Author JoeBig7
+ * @date 2021/2/5 20:01:22
+ */
+public class ExcelDataVo {
+
+
+    private String customerId;
+
+    private String channel;
+
+
+    private String studioName;
+
+    private String number;
+
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getStudioName() {
+        return studioName;
+    }
+
+    public void setStudioName(String studioName) {
+        this.studioName = studioName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
